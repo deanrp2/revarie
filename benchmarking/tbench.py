6834c8a574898652dc75b57c9c1300588989819e
deanrp2 <deanrp2@illinois.edu>
2020-05-01 09:18:36 -0500

    Added benchmarking directory

---
+++ benchmarking/tbench.py
@@ -0,0 +1,42 @@
+import sys
+import time
+import numpy as np
+
+sys.path.append("../")
+
+from revarie import Revarie
+
+
+# --- need to do time
+# --- need to do memory
+
+nug = 0
+sill = 1
+rang = 0.5
+
+mu = 0
+
+dom_upper = 1
+dom_lower = 0
+
+ptnums = np.linspace(1000,4000, 200).astype(np.int)
+
+times = np.zeros_like(ptnums).astype(np.float64)
+trials = np.zeros(8)
+for j, pn in enumerate(ptnums):
+    x = np.zeros((pn, 2))
+    x[:,1] = np.random.uniform(0,1,pn)
+    x[:,0] = np.random.uniform(0,1,pn)
+    for i in range(8):
+        start = time.time()
+        t = Revarie(x,mu,nug,sill,rang,"sph")
+        trials[i] = time.time() - start
+    times[j] = trials.mean()
+    
+with open("2D_bench_2020-4-30.dat","w") as f:
+    for pn in ptnums:
+        f.write("%i"%pn + ",")
+    f.write("\n")
+    for t in times:
+        f.write("%.8E"%t + ",")
+
---
+++ benchmarking/tbench_proc.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as so
+import datetime
+
+infile = "2D_bench_2020-4-30.dat"
+infile = "1D_bench_2020-4-29.dat"
+
+with open(infile, "r") as f:
+    d = f.read().splitlines()
+    
+n = np.array(d[0].split(",")[:-1]).astype(np.float64)
+times = np.array(d[1].split(",")[:-1]).astype(np.float64)
+
+r = so.curve_fit(lambda n, p, k:k*(n)**p, n, times)[0]
+
+print("For k(n)^p: k = {k}   p = {p}".format(k=r[1],p=r[0]))
+
+
+#%%
+#predict runtime
+n = 10000
+print("Predicted runtime for {n} points is {s}".format(n=n, s =str(datetime.timedelta(seconds = r[1]*n**r[0]))  ))
+
+#plt.loglog(n, times,".",c="k")
