9ad4bab945be6bbf37f050d5ab56db4e92d3256c
deanrp2 <deanrp2@illinois.edu>
2020-05-01 12:32:25 -0500

    reconfigured to include runs with generation of random fields

--- benchmarking/tbench_proc.py
+++ benchmarking/tbench_proc.py
@@ -4,7 +4,7 @@ import scipy.optimize as so
 import datetime
 
 infile = "2D_bench_2020-4-30.dat"
-infile = "1D_bench_2020-4-29.dat"
+infile = "1D_bench_2020-5-1.dat"
 
 with open(infile, "r") as f:
     d = f.read().splitlines()
@@ -19,7 +19,7 @@ print("For k(n)^p: k = {k}   p = {p}".format(k=r[1],p=r[0]))
 
 #%%
 #predict runtime
-n = 10000
+n = 12500
 print("Predicted runtime for {n} points is {s}".format(n=n, s =str(datetime.timedelta(seconds = r[1]*n**r[0]))  ))
 
 #plt.loglog(n, times,".",c="k")
