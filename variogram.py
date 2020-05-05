b99da7795fe5dabde1952ee923cf4ce94880f9d3
deanrp2 <deanrp2@illinois.edu>
2020-05-05 16:09:26 -0500

    added init conditioning method

--- variogram.py
+++ variogram.py
@@ -3,35 +3,59 @@ import matplotlib.pyplot as plt
 from scipy.spatial.distance import pdist
 
 class Exp_Variogram:
-    def __init__(x, f, bins = []):
-        self.x = self.x
-        self.f = self.f
+    def __init__(self, x, f, bins, bin_type):
+        """
+        bin_type: "lin" (minlag, maxlag, nlag) min and max lag 
+        samewidth as others, "bound","auto"
+        """
         self.check_init()
 
-        self.set_bins(bins = bins)#list of bin centers, list of bin boundaries, 
-        #               or empty
-
+        self.x = x
+        self.f = f
         self.s = f.size
+
+        self.cond_init()
+
+        self.lags = self.calc_lags()
+
+        self.set_bins(bins, bin_type)
+
+
+    def set_bins(self, bins, bin_type):
+        if bin_type == "lin":
+            db = (bins[1] - bins[0])/(bins[2] - 1)
+            self.bin_bounds = np.lispace(bins[0],bins[1],bins[2]+1)-db/2
+        elif bin_type == "bound":
+            self.bin_bounds = np.asarray(bins)
+        elif bin_type == "auto":
+            print("auto not yet implimented")
+
+    def calc_lags(self):
+        return pdist(self.x)
 
     def check_init(self):
         #check object for valid creation
         # --- x and f same size
         # --- x and f numpy arrays
         # --- f is 0D
+        # --- check that bins and bintype line up
+        # --- check if bincenters make any negative lags
+        # --- check order of bounds and cent
         return 0
 
-    def set_bins(self, bins):
-        if bins is []:
-            #do something
-        else:
-            bins = np.array(bins)
-            if bins.shape
+    def cond_init(self):
+        self.x = np.asarray(self.x)
+        self.f = np.asarray(self.f)
 
-            elif:
+        if self.x.ndim < 2:
+            self.x = self.x.reshape(x.size,1)
+        #other object type changes and such
+
+
 
 
 if __name__ == "__main__":
     x = np.linspace(0,1,100)
     f = x**2
 
-    test = Exp_Variogram(x,f)
+    test = Exp_Variogram(x,f, (1,10,10) ,"lin")
