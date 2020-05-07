8aa6f867d7e63ae0c9cdb900bf678b2197bf2ebf
deanrp2 <deanrp2@illinois.edu>
2020-05-07 16:38:37 -0500

    changed variable nomenclature to be consistant, fixed gaussian model

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 import numpy as np
 
-def spherical(h, c0, r, b):
-    variogram = b + c0*np.piecewise(h, [h <= r,  h > r],[lambda h: 1.5*(h/r)-0.5*(h/r)**3,1])
-    return c0 - variogram
+def spherical(h, nug, sill, rang):
+    variogram = nug + sill*np.piecewise(h, [h <= rang,  h > rang],[lambda h: 1.5*(h/rang)-0.5*(h/rang)**3,1])
+    return sill - variogram
 
-def exponential(h, c0, r, b):
-    return b + c0 * (1. - np.exp(-3*h /r))
+def exponential(h, nug, sill, rang):
+    variogram = nug +sill*(1.-np.exp(-3*h/rang))
+    return sill - variogram
 
-def gaussian(h, c0, r, b):
-    return b + c0 * (1. - np.exp(-2*h/r))
+def gaussian(h, nug, sill, rang):
+    variogram =  nug+sill*(1.-np.exp(-(2*h/rang)**2))
+    return sill - variogram
