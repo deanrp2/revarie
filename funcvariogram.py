ec649899f672350828a349ee0bc90d10ed3279cc
deanrp2 <deanrp2@illinois.edu>
2020-05-12 15:57:30 -0500

    Added file with imports of func variogram

---
+++ funcvariogram.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import functools
+import warnings
+
+class FuncVariogram:
+    """
+
+    Data structure to store functional form of variogam defined in a variety
+    of ways.
+
+    """
+    def __init__(*args, **kwargs):
+        print("k")
+
