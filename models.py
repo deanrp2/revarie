80dba8944b4f734d8c4986f4bdcaeee624ddb6c5
deanrp2 <deanrp2@illinois.edu>
2020-05-16 11:50:10 -0500

    changed return from covariance to variogram, will break revarie class

--- models.py
+++ models.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def spherical(h, nug, sill, rang):
     variogram = nug + sill*np.piecewise(h, [h <= rang,  h > rang],[lambda h: 1.5*(h/rang)-0.5*(h/rang)**3,1])
-    return sill - variogram
+    return variogram
 
 def exponential(h, nug, sill, rang):
     variogram = nug +sill*(1.-np.exp(-3*h/rang))
-    return sill - variogram
+    return variogram
 
 def gaussian(h, nug, sill, rang):
     variogram =  nug+sill*(1.-np.exp(-(2*h/rang)**2))
-    return sill - variogram
+    return variogram
