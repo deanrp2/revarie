b1a3780c1b690c598c4c6053d3f0450360d7c223
deanrp2 <deanrp2@illinois.edu>
2020-05-17 08:35:05 -0500

    changed class structure to series of functions

--- funcvariogram.py
+++ funcvariogram.py
@@ -8,96 +8,64 @@ from scipy.interpolate import interp1d
 from scipy.optimize import curve_fit
 from .models import *
 
-class FuncVariogram:
+def fvariogram(source, method, options, *args, **kwargs):
+    if source == "func":
+        if method == "ufunc":
+            return options[0]
+        else:
+
+            if method == "sph":
+                f = spherical
+            elif method == "exp":
+                f = exponential
+            elif method == "gaus":
+                f = gaussian
+
+            _f = lambda *a : f(*a[::-1])
+            return partial(_f, *options[::-1])
+
+    elif source == "data":
+        h = options[0]
+        v = options[1]
+
+        if method == "interp":
+            return interp(h, v, options[2], *args, **kwargs)
+        elif method == "poly":
+            return polyfit(h, v, options[2], *args, **kwargs)
+        elif method == "bmodel":
+            return bmodel_fit(h, v, options[2], *args, **kwargs)
+        elif method == "umodel":
+            return umodel_fit(h, v, options[2], *args, **kwargs)
+
+def interp(h, v, kind, *args, **kwargs):
     """
-
-    Data structure to store functional form of variogam defined in a variety
-    of ways.
-
+    Highl reccomended that copy=False is used
     """
-    def __init__(self,
-                 source,
-                 method,
-                 options,
-                 *args, **kwargs):
+    return interp1d(h, v, kind, *args, **kwargs)
 
-        self.source = source
-        self.method = method
+def polyfit(h, v, order, *args, **kwargs):
+    P = np.polyfit(h,v,order, *args, **kwargs)
 
+    def f(h):
+        return np.polyval(P, h)
 
-        if source == "func":
-            self.irange = (-np.inf, np.inf)
+    return f
 
-            if method == "ufunc":
-                self.f = options[0]
-            else:
+def bmodel_fit(h, v, model, *args, **kwargs):
+    """
+    Current avaliable models = sph, exp, gaus
+    """
+    if model == "sph":
+        m = spherical
+    elif model == "exp":
+        m = exponential
+    elif model == "gaus":
+        m = gaussian
 
-                if method == "sph":
-                    f = spherical
-                elif method == "exp":
-                    f = exponential
-                elif method == "gaus":
-                    f = gaussian
-
-                _f = lambda *a : f(*a[::-1])
-                self.f = partial(_f, *options[::-1])
-
-        elif source == "data":
-            if len(options) == 4:
-                self.irange = options[3]
-            else:
-                self.irange = (np.min(options[0]), np.max(options[0]))
-
-            h = options[0]
-            v = options[1]
-
-            if method == "interp":
-                self.f = self.interp(h, v, options[2], *args, **kwargs)
-            elif method == "poly":
-                self.f = self.polyfit(h, v, options[2], *args, **kwargs)
-            elif method == "bmodel":
-                self.f = self.bmodel_fit(h, v, options[2], *args, **kwargs)
-            elif method == "umodel":
-                self.f = self.umodel_fit(h, v, options[2], *args, **kwargs)
-
-    def interp(self, h, v, kind, *args, **kwargs):
-        """
-        Highl reccomended that copy=False is used
-        """
-        return interp1d(h, v, kind, *args, **kwargs)
-
-    def polyfit(self, h, v, order, *args, **kwargs):
-        P = np.polyfit(h,v,order, *args, **kwargs)
-
-        def f(h):
-            return np.polyval(P, h)
-
-        return f
-
-    def bmodel_fit(self, h, v, model, *args, **kwargs):
-        """
-        Current avaliable models = sph, exp, gaus
-        """
-        if model == "sph":
-            m = spherical
-        elif model == "exp":
-            m = exponential
-        elif model == "gaus":
-            m = gaussian
-
-        return self.umodel_fit(h, v, m, *args, **kwargs)
+    return umodel_fit(h, v, m, *args, **kwargs)
 
 
-    def umodel_fit(self, h, v, f, *args, **kwargs):
-        opts, _ = curve_fit(f, h, v, *args, **kwargs)
-        _f = lambda *a : f(*a[::-1])
-        return partial(_f, *opts[::-1])
-
-    def echeck(self, x):
-        if not self.extra:
-            if not np.all(np.logical_and(x > self.range[0],
-                                         x < self.range[1])):
-                raise Exception("Extrapolation detected, change extrapola"
-                    "tion parameter to True if this error is undesired")
-
-
+def umodel_fit(h, v, f, *args, **kwargs):
+    opts, _ = curve_fit(f, h, v, *args, **kwargs)
+    _f = lambda *a : f(*a[::-1])
+    return partial(_f, *opts[::-1])
