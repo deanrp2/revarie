1f84e8176807e20d1de37fc09f3269c3debc2465
deanrp2 <deanrp2@illinois.edu>
2020-05-31 14:49:02 -0500

    Worked more on function to print results of benchmarking test

--- benchmarking/output.py
+++ benchmarking/output.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import curve_fit
 import platform, subprocess
+from datetime import datetime, date
 
 def get_processor_info():
     if platform.system() == "Windows":
@@ -11,11 +12,24 @@ def get_processor_info():
     return r.decode("utf-8")
 
 
-def write(times, n, fname):
+def write(n, t, fname, typ):
+    """
+    typ should be "variogram" or "revarie"
+    """
     out = open(fname, "w")
-    out.write("% --- CPU info\n\n")
+    out.write("% --- CPU info\n")
     out.write(get_processor_info() + "\n\n")
+
+    out.write("% --- Test Info\n")
+    out.write("Test Date/Time:".ljust(24) + str(datetime.now()) + "\n")
+    out.write("Test Type:".ljust(24) + typ + "\n")
+    out.write("Nrange:".ljust(24) + str(n.min()) + "-" + str(n.max()))
+
+    out.write("\n\n% --- Timing Data\n")
+    #for x1, x2 in zip(
 
 
 if __name__ == "__main__":
-    print(get_processor_info())
+    n = np.array([1,2,3,4,5,6,7,8,9,10])
+    t = np.exp(n)
+    write(n, t, "t.dat", "variogram")
