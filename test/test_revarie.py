8e319b5d286c72ae60bf3d28d7561e24c6490887
deanrp2 <deanrp2@illinois.edu>
2020-05-31 13:43:01 -0500

    Added docs for unittesting

--- test/test_revarie.py
+++ test/test_revarie.py
@@ -4,6 +4,9 @@ import numpy as np
 
 class TestRevarie(unittest.TestCase):
     def test_init(self):
+        """
+        Test if any errors in initialization of Revarie instance
+        """
         x = np.random.uniform(-1,1,(10,2))
         m = lambda h : np.sqrt(h)
 
@@ -12,6 +15,9 @@ class TestRevarie(unittest.TestCase):
         self.assertTrue(r.mu == 0)
 
     def test_trivial(self):
+        """
+        Make sure field generation in trivial case is correct
+        """
         x = np.random.uniform(-1,1, (5,2))
         m = lambda h : 0
 
@@ -20,6 +26,9 @@ class TestRevarie(unittest.TestCase):
         self.assertTrue(np.allclose(r.genf(), 1))
 
     def test_shape(self):
+        """
+        Make sure length of generated field data array is same length of x
+        """
         x = np.random.uniform(-1,1, (10,2))
         m = lambda h : 0
 
@@ -27,6 +36,9 @@ class TestRevarie(unittest.TestCase):
         self.assertTrue(r.genf().size, x.shape[0])
 
     def test_cov(self):
+        """
+        Test generation of covariance matrix for trivial case
+        """
         x = np.linspace(0,10,10)
         m = lambda h : 1
 
