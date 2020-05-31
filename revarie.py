c0de9c0e5d34e03234efa8bdaf3c2c39e81a0916
deanrp2 <deanrp2@illinois.edu>
2020-05-31 08:22:03 -0500

    Added docstrings

--- revarie.py
+++ revarie.py
@@ -9,6 +9,26 @@ from .fvariogram import *
 
 class Revarie:
     def __init__(self, x, mu, sill, model):
+        """
+        Class to generate random fields based on a variogram given as a
+        function in the 'model' parameter, mean and variance for a number of
+        points given in the x parameter.
+
+        Parameters
+        ----------
+        x : numpy.ndarray
+            Array of shape (m,n) where n is the number of points in an
+            m-dimensional domain. Each row is a point. This does not need to
+            be the same points used to calculate the original variogram.
+        mu : float
+            Spatially-independent mean of field values
+        sill : float
+            Spatially-independent variance of field values
+        model : function
+            Callable with takes numpy array of lag distances as argument and
+            returns numpy array of variogram values. Should only take a single
+            parameter.
+        """
         self.x = x
         self.mu = mu
         self.sill = sill
@@ -20,6 +40,9 @@ class Revarie:
         self.genf(x, model)
 
     def genf(self, x, model):
+        """
+        Creates the covariance matrix for the points with the variogram model
+        """
         if x.ndim < 2:
             x = x.reshape(x.size, 1)
 
@@ -34,7 +57,19 @@ class Revarie:
         self.cov = np.maximum(h_cov,h_cov.T)
 
     def mnorm(self):
-        return np.random.multivariate_normal(np.ones(self.s)*self.mu, self.cov)
+        """
+        Generates random field values using covariance matrix calculated
+        previously.
+
+        Returns
+        -------
+        fvs : numpy array
+            Array of field values of length x.shape[0] that correspond to each
+            point in x
+        """
+
+        fvs = np.random.multivariate_normal(np.ones(self.s)*self.mu, self.cov)
+        return fvs
 
     def check_init(self):
         if not callable(self.model):
