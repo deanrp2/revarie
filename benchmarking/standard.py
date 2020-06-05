ed8bda4d37b645d5cfb89d73c2660622f348c6b3
deanrp2 <deanrp2@illinois.edu>
2020-06-05 09:12:15 -0500

    added suite function and revarie test

--- benchmarking/standard.py
+++ benchmarking/standard.py
@@ -1,17 +1,16 @@
+from revarie import Revarie
 from revarie import Variogram
+from revarie import fvariogram
 import numpy as np
 import time
 import datetime
 from .output import write
 from pathlib import Path
 
-def bench_variogram(path = "."):
+def bench_variogram(tlimit = 15, path = "."):
     tot_time = time.time()
 
-    ttag = datetime.datetime.now()
-    ttag = ttag.strftime("%Y_%m_%d")
     n = 50
-    tlimit = 15 #s
     ns = []
     ts = []
     while time.time() - tot_time < tlimit:
@@ -25,5 +24,49 @@ def bench_variogram(path = "."):
         ts.append(time.time() - start)
         n = int(n*1.2)
 
-    fname = Path("variogram_timing-" + ttag + ".dat")
+    t = 0
+    fname = Path("variogram_timing" + str(t).zfill(3) + ".dat")
+    while (Path(path) / fname).is_file():
+        t += 1
+        fname = Path("variogram_timing" + str(t).zfill(3) + ".dat")
     write(np.asarray(ns),np.asarray(ts),Path(path) / fname,"Variogram", "1-D")
+
+def bench_revarie(tlimit = 15, path = "."):
+    tot_time = time.time()
+
+    nug = 0
+    sill = 1
+    rang = 0.3
+    v = fvariogram("func", "sph", [nug, sill, rang])
+
+    mu = 1
+
+    n = 50
+    ns = []
+    ts = []
+    while time.time() - tot_time < tlimit:
+        start = time.time()
+        ns.append(n)
+        x = np.random.uniform(0,1,n)
+
+        Revarie(x, mu, sill, v).genf()
+
+        ts.append(time.time() - start)
+        n = int(n*1.2)
+
+    t = 0
+    fname = Path("revarie_timing" + str(t).zfill(3) + ".dat")
+    while (Path(path) / fname).is_file():
+        t += 1
+        fname = Path("revarie_timing" + str(t).zfill(3) + ".dat")
+    write(np.asarray(ns),np.asarray(ts),Path(path) / fname,"Revarie", "1-D")
+
+def suite(tlimit = 30, path = "."):
+    bench_variogram(tlimit/2, path = path)
+    bench_revarie(tlimit/2, path = path)
+
+
+
+
+
+
