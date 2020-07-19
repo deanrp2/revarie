e4383ad3a94b327baa0f9ecd7ede46333e67281e
deanrp2 <deanrp2@illinois.edu>
2020-07-19 12:51:43 -0500

    fixed variogarm models to be correct for nonzero nuggets

--- revarie/models.py
+++ revarie/models.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 def spherical(h, nug, sill, rang):
-    variogram = nug + sill*np.piecewise(h, [h <= rang,  h > rang],[lambda h: 1.5*(h/rang)-0.5*(h/rang)**3,1])
+    variogram = nug + (sill - nug)*np.piecewise(h, [h <= rang,  h > rang],[lambda h: 1.5*(h/rang)-0.5*(h/rang)**3,1])
     return variogram
 
 def exponential(h, nug, sill, rang):
-    variogram = nug +sill*(1.-np.exp(-3*h/rang))
+    variogram = nug + (sill - nug)*(1.-np.exp(-3*h/rang))
     return variogram
 
 def gaussian(h, nug, sill, rang):
-    variogram =  nug+sill*(1.-np.exp(-(2*h/rang)**2))
+    variogram =  nug + (sill - nug)*(1.-np.exp(-(2*h/rang)**2))
     return variogram
 
 mtags = {"sph" : spherical,
